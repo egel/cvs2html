7ebf9eb500f1137a1511e14b4b48df962cfe3218
Maciej Sypien <maciejsypien@gmail.com>
2014-04-28 22:22:46 +0200

    Improved with a bunch of new functions; Separate logic;

--- cvs2html.py
+++ cvs2html.py
@@ -1,31 +1,76 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import sys
 import os
 
-version = "1.0"
+__version__ = "1.0"
 program_name = os.path.basename(__file__)
 authors = "Maciej Sypień"
 
 
 def main():
-    maxwidth = 100
-    print_start()
-    count = 0
-    while True:
+    if len(sys.argv) > 1:
         try:
-            line = input()
-            if count == 0:
-                color = "lightgreen"
-            elif count % 2:
-                count = "white"
+            process_questions()
+            options = process_options()
+            maxwidth = 100
+
+            if len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
+                path_to_file = sys.argv[1]
+                array = get_linelist_from_file(path_to_file)
+                print_start()
+                count = 0
+                for line in array:
+                    try:
+                        print(line)
+                        """
+                        if count == 0:
+                            color = "lightgreen"
+                        elif count % 2:
+                            count = "white"
+                        else:
+                            count = "lightyellow"
+                        print_line(line, color, maxwidth)
+                        count += 1
+                        """
+                    except EOFError:
+                        break
+                print_end()
+            elif len(sys.argv) == 3 and sys.argv[2] == ">":
+                print("not done yet")
             else:
-                count = "lightyellow"
-            print_line(line, color, maxwidth)
-            count += 1
-        except EOFError:
-            break
-    print_end()
+                s = ("Wrong usage.\n"
+                     "Use '{0} --help' for help").format(program_name)
+                print(s)
+        except ValueError as err:
+            print(err)
+    else:
+        s = ("Nothing happen.\n"
+             "Use '{0} --help' for help.").format(program_name)
+        print(s)
+
+
+def process_options():
+    pass
+
+
+def process_questions():
+    if sys.argv[1] in ("-h", "--help"):
+        s = ("{0:-^50}\n"
+             "Example usage:\n"
+             "$ {2} [PATH]\n"
+             "{1:-<50}".format(" Help ", "", program_name))
+        print(s)
+        sys.exit()
+    elif sys.argv[1] in ("-V", "--__version__"):
+        s = ("{0}  v{1}\n"
+             "License GPLv3+: GNU GPL __version__ 3 or later "
+             "http://gnu.org/licenses/gpl.html\n"
+             "\n"
+             "Author: {2}").format(program_name, __version__, authors)
+        print(s)
+        sys.exit()
 
 
 def print_start():
@@ -34,6 +79,28 @@ def print_start():
 
 def print_end():
     print('</table>')
+
+
+def print_line(line, color, maxwidth=100):
+    print('<tr style="background: {0}">'.format(color))
+    fields = extract_fields(line)
+    for field in fields:
+        if not field:
+            print("<td></td>")
+        else:
+            number = field.replace(",", "")
+            try:
+                x = float(number)
+                print('<td align="right"{0:d}</td>'.format(round(x)))
+            except ValueError:
+                field = field.title()
+                field = field.replace(" And ", " and ")
+                if len(field) <= maxwidth:
+                    field = escape_html(field)
+                else:
+                    field = "{0} ...".format(escape_html(field[:maxwidth]))
+                print("<td>{0}</td>".format(field))
+    print("</tr>")
 
 
 def extract_fields(line):
@@ -59,28 +126,6 @@ def extract_fields(line):
     return fields
 
 
-def print_line(line, color, maxwidth=100):
-    print('<tr style="background: {0}">'.format(color))
-    fields = extract_fields(line)
-    for field in fields:
-        if not field:
-            print("<td></td>")
-        else:
-            number = field.replace(",", "")
-            try:
-                x = float(number)
-                print('<td align="right"{0:d}</td>'.format(round(x)))
-            except ValueError:
-                field = field.title()
-                field = field.replace(" And ", " and ")
-                if len(field) <= maxwidth:
-                    field = escape_html(field)
-                else:
-                    field = "{0} ...".format(escape_html(field[:maxwidth]))
-                print("<td>{0}</td>".format(field))
-    print("</tr>")
-
-
 def escape_html(text):
     text = text.replace("&", "&amp;")
     text = text.replace("<", "&lt;")
@@ -88,27 +133,26 @@ def escape_html(text):
     return text
 
 
+def get_linelist_from_file(path_to_file):
+    try:
+        ins = open(path_to_file, "r")
+    except IOError:
+        print('File does\'t exsist.')
+    array = []
+    for line in ins:
+        array.append(line)
+    ins.close()
+    return array
+
+
+def is_cvs_file(path_to_file):
+    fileName, fileExtension = os.path.splitext(path_to_file)
+    if fileExtension == '.cvs':
+        return True
+    else:
+        return False
+
+
 # Begin of program
 # print(sys.version)
-if len(sys.argv) > 1:
-    try:
-        if sys.argv[1] in ("-h", "--help"):
-            s = ("{0:-^50}\n"
-                 "Example usage:\n"
-                 "$ {2} [PATH]\n"
-                 "{1:-<50}".format(" Help ", "", program_name))
-            print(s)
-        elif sys.argv[1] in ("-V", "--version"):
-            s = ("{0}  v{1}\n"
-                 "License GPLv3+: GNU GPL version 3 or later "
-                 "http://gnu.org/licenses/gpl.html\n"
-                 "\n"
-                 "Author: {2}").format(program_name, version, authors)
-            print(s)
-        else:
-            main()
-    except ValueError as err:
-        print(err)
-else:
-    print("Nothing happen. Use '{0} --help' for help".format(
-        os.path.basename(__file__)))
+main()
